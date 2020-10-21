18d311835ce52462508456b9cf313f3f863207da
Altieres Schincariol <69064167+altnetto@users.noreply.github.com>
2020-10-21 16:50:14 -0300

    Add files via upload
    Vs 1.0
    
    - Interface visual implementada;
    - Helper para limpar a tela implementado;
    - Erros no List<string>

---
+++ TP6SchincariolNettoAltieres/Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.txtPaginas = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cbxImportado = new System.Windows.Forms.CheckBox();
+            this.cbxGenero = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabIngreso = new System.Windows.Forms.TabPage();
+            this.tabLista = new System.Windows.Forms.TabPage();
+            this.lbxLista = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabIngreso.SuspendLayout();
+            this.tabLista.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Autor";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Género";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(90, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Páginas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(90, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Importado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(90, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Precio";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(163, 47);
+            this.txtNombre.MaxLength = 32;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 7;
+            this.txtNombre.Tag = "txtNombre";
+            this.txtNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(163, 87);
+            this.txtAutor.MaxLength = 32;
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(100, 20);
+            this.txtAutor.TabIndex = 7;
+            // 
+            // txtPaginas
+            // 
+            this.txtPaginas.Location = new System.Drawing.Point(163, 167);
+            this.txtPaginas.MaxLength = 5;
+            this.txtPaginas.Name = "txtPaginas";
+            this.txtPaginas.Size = new System.Drawing.Size(100, 20);
+            this.txtPaginas.TabIndex = 7;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(163, 247);
+            this.txtPrecio.MaxLength = 15;
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Cancelar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 394);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Ingresar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // cbxImportado
+            // 
+            this.cbxImportado.AutoSize = true;
+            this.cbxImportado.Location = new System.Drawing.Point(163, 209);
+            this.cbxImportado.Name = "cbxImportado";
+            this.cbxImportado.Size = new System.Drawing.Size(37, 17);
+            this.cbxImportado.TabIndex = 11;
+            this.cbxImportado.Text = "Sí";
+            this.cbxImportado.UseVisualStyleBackColor = true;
+            this.cbxImportado.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // cbxGenero
+            // 
+            this.cbxGenero.FormattingEnabled = true;
+            this.cbxGenero.Items.AddRange(new object[] {
+            "Aventura",
+            "Autoayuda",
+            "Autobiografía",
+            "Biografía",
+            "Crónicas",
+            "Fantasía",
+            "Ficción Científica",
+            "Historia",
+            "Poesía"});
+            this.cbxGenero.Location = new System.Drawing.Point(163, 127);
+            this.cbxGenero.Name = "cbxGenero";
+            this.cbxGenero.Size = new System.Drawing.Size(100, 21);
+            this.cbxGenero.TabIndex = 12;
+            this.cbxGenero.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnIngresar);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.cbxImportado);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.cbxGenero);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtAutor);
+            this.groupBox1.Controls.Add(this.txtPaginas);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(0, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(375, 329);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingresar Libro";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(188, 288);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 14;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(93, 288);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 13;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabIngreso);
+            this.tabControl1.Controls.Add(this.tabLista);
+            this.tabControl1.Location = new System.Drawing.Point(3, 1);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(383, 358);
+            this.tabControl1.TabIndex = 13;
+            // 
+            // tabIngreso
+            // 
+            this.tabIngreso.Controls.Add(this.groupBox1);
+            this.tabIngreso.Location = new System.Drawing.Point(4, 22);
+            this.tabIngreso.Name = "tabIngreso";
+            this.tabIngreso.Padding = new System.Windows.Forms.Padding(3);
+            this.tabIngreso.Size = new System.Drawing.Size(375, 332);
+            this.tabIngreso.TabIndex = 0;
+            this.tabIngreso.Text = "Ingreso";
+            this.tabIngreso.UseVisualStyleBackColor = true;
+            // 
+            // tabLista
+            // 
+            this.tabLista.Controls.Add(this.lbxLista);
+            this.tabLista.Location = new System.Drawing.Point(4, 22);
+            this.tabLista.Name = "tabLista";
+            this.tabLista.Padding = new System.Windows.Forms.Padding(3);
+            this.tabLista.Size = new System.Drawing.Size(375, 332);
+            this.tabLista.TabIndex = 1;
+            this.tabLista.Text = "Lista";
+            this.tabLista.UseVisualStyleBackColor = true;
+            // 
+            // lbxLista
+            // 
+            this.lbxLista.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbxLista.FormattingEnabled = true;
+            this.lbxLista.Location = new System.Drawing.Point(6, 10);
+            this.lbxLista.Name = "lbxLista";
+            this.lbxLista.Size = new System.Drawing.Size(359, 316);
+            this.lbxLista.TabIndex = 0;
+            this.lbxLista.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Ingresar Libros";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabIngreso.ResumeLayout(false);
+            this.tabLista.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.TextBox txtPaginas;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox cbxImportado;
+        private System.Windows.Forms.ComboBox cbxGenero;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabLista;
+        private System.Windows.Forms.TabPage tabIngreso;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.ListBox lbxLista;
+    }
+}
+
---
+++ TP6SchincariolNettoAltieres/Ejercicio1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public List<string> listaLibros = new List<string>();
+
+        public void limpiarPantalla()
+        {
+            txtNombre.Text = "";
+            txtAutor.Text = "";
+            cbxGenero.Text = "";
+            txtPaginas.Text = "";
+            cbxImportado.Checked = false;
+            txtPrecio.Text = "";
+        }
+        public string cadastrarLibro()
+        {
+            string Nombre = txtNombre.Text + " - ";
+            string Autor = txtAutor.Text + " - ";
+            string Genero = cbxGenero.Text + " - ";
+            string Paginas = txtPaginas.Text + " - ";
+            string Precio = txtPrecio.Text;
+            string Importado;
+
+            campoVacio(txtNombre);
+            campoVacio(txtAutor);
+            campoVacio(txtPaginas);
+            campoVacio(txtPrecio);
+
+            if (cbxImportado.Checked == false)
+            {
+                Importado = "Nacional - ";
+            }
+            else
+            {
+                Importado = "Importado - ";
+            }
+
+
+            return (Nombre +
+                    Autor +
+                    Genero +
+                    Paginas +
+                    Importado +
+                    Precio);
+        }
+
+        public void campoVacio (TextBox campo)
+        {
+            if(campo.Text == "")
+            {
+                MessageBox.Show("Hay un campo vacio!");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            limpiarPantalla();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            lbxLista.DataSource = listaLibros;
+            string nuevoItemLista = cadastrarLibro();         
+            listaLibros.Add(nuevoItemLista);
+            limpiarPantalla();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
